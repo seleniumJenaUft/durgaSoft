d5f169c3391f784a589e995c8b85eb0f62d030f2
Bishal <bishal1503@gmail.com>
2022-08-15 06:45:12 +0530

    Initial Commit

---
+++ src/datatype/Data.java
@@ -0,0 +1,41 @@
+package datatype;
+
+public class Data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//	inte -4 byte
+//		byte -1 byte
+//		short - 2 byte
+//		float - 5 to 6 decimal place accuracy- 4 byte - -1.7*10^38 - 1.7
+//		double - 14 to 15 decimal place accuracy- 8 byte- 3.4*10^308
+//		long - 8 byte- -2^63 to 2^63
+//		boolean - virtual machine dependent - size is not defined , 
+//					no range , allowed value- true, false- default - false
+//		char - 2 byte java -unicode based , old language - ascii code value- default - space
+//		 null is not applicable to primitive data type , only for objects 
+	boolean b=true;	
+//	if number prifixed with 0 then considered octal
+	int O1 =010;
+//	if number proceed 0X then hexadecimal
+	int h1=0X10;// java not case sensitive with the alphabets a to f for hexa
+	long h11=0X10l;
+	int h2=0xFace;
+	int h3=h1+h2;
+	float f1=1.1233F;// float has to be explicitly defined in the literal
+	float f11=1.1233f;
+	double d1=1.123D; // double can be specified with D/d but this is not required as its default
+	double d=0123.123456;// this is changed to double decimal and its not octal.. even hexadecimal is not allowed
+	
+	System.out.println(O1);
+	System.out.println(h1);
+	System.out.println(h2);
+	System.out.println(h3);
+	System.out.println(h11);
+	
+System.out.println(.2+.7);
+System.out.println(.2+.1);
+System.out.println(.2+.3);
+	}
+
+}
---
+++ src/datatype/ErrorData.java
@@ -0,0 +1,33 @@
+package datatype;
+
+public class ErrorData {
+
+	public static void main(String[] args) {
+//		int 4 byte
+//		byte -1 byte
+//		short - 2 byte
+//		float - 5 to 6 decimal place accuracy- 4 byte - -1.7*10^38 - 1.7
+//		double - 14 to 15 decimal place accuracy- 8 byte- 3.4*10^
+//		long - 8 byte- -2^63 to 2^63
+//		boolean - virtual machine dependent - size is not defined , 
+//					no range , allowed value- true, false
+		byte b =128;
+		byte c=1.123;
+		byte v=true;
+		boolean r=True;
+		boolean t="true" ;
+		float f1=123.2345;//have to explicityly define as float with F;
+//		float f2=0X12.1234f;
+//		double d23=0X123.13434;// compile time first error
+				
+		int x=0;
+		if(x)
+			System.out.println("false");
+		else 
+			System.out.println("true");
+//================== literals
+		int xtr =086;// 8 is not allowed in octal
+		int wer=0xbeer;// r is not allwed in hexa
+	}
+
+}
